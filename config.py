ff1efe4a343059e8842795026bed5ebb679a8a83
Rajesh1st <rajeshtb55@outlook.com>
2025-02-25 15:37:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ import os
 
 class Config(object):
     BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-    API_ID = int(os.environ.get("APP_ID", ))
+    API_ID = int(os.environ.get("APP_ID", "17584090"))
     API_HASH = os.environ.get("API_HASH", "")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
     MONGODB_URI = os.environ.get("MONGODB_URI", "")
     SESSION_NAME = os.environ.get("SESSION_NAME", "")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", ))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", "7252430326"))
